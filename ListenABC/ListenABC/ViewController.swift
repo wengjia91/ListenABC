06bb7b4af9faab21dba91f2c4437a51bc3c998ca
baiyang <baiyang@in66.com>
2015-12-22 00:19:19 +0800

    初始化工程，使用CocoaPod 添加 Alamofire 和 SnapKit

---
+++ ListenABC/ListenABC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListenABC
+//
+//  Created by 赖隆斌 on 15/12/21.
+//  Copyright © 2015年 owl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
